4fd5a8b75d59a66cf0e110207582a408214b0ad9
ub9fbi <zolotoianatolji@yandex.com>
2023-09-04 12:44:10 +0500

    12

--- Lessons4/ArraysTheme.java
+++ Lessons4/ArraysTheme.java
@@ -1,5 +1,8 @@
 package Lessons4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class ArraysTheme {
     public static void main(String[] args) {
         int[] numbs = {1, 7, 4, 5, 2, 6, 3};
@@ -13,5 +16,17 @@ public class ArraysTheme {
             numbs[6] = 1;
             System.out.print(numbs[i] + " ");
         }
+
+        Random random = new Random();                   // генерация вещественных чисел
+        double[] numbDoubl = new double[15];
+
+        for (int i = 0; i < numbDoubl.length; i++) {    // заполнение массива вещественными числами
+            numbDoubl[i] = random. nextDouble();
+        }
+        for (double number : numbDoubl) {               // вывод массива на экран
+            System.out.format("%.2f%n" + " ", number );
+        }
+
+
     }
 }
